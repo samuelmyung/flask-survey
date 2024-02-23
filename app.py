376ad9a63324721029e118571fd4315610ed5c58
David Sapiro <dsap48@gmail.com>
2024-02-22 17:58:46 -0800

    finished again

--- app.py
+++ app.py
@@ -8,40 +8,50 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-RESPONSES = []
 
 
 @app.get("/")
 def create_homepage():
     """Create homepage with a start of a survey"""
-    RESPONSES.clear()
-    return render_template("survey_start.html", survey_title=survey.title,
-                           survey_instructions=survey.instructions)
+    session['responses'] = []
 
+    return render_template(
+        "survey_start.html",
+        survey_title=survey.title,
+        survey_instructions=survey.instructions
+    )
 
 @app.post("/begin")
 def change_page():
     """Redirects us to the first question"""
+    session['responses'].clear()
     return redirect("/questions/0")
 
 
 @app.get("/questions/<int:index>")
 def create_question_page(index):
-    """Create each subsequent question page and redirects to thank you page if
-    no more questions"""
-    if index >= len(survey.questions):
+    """Creates question page and will redirects to correct question or the
+    thank you page if all questions have been answered."""
+    if len(session['responses']) == len(survey.questions):
         return redirect("/thank_you")
 
+    if len(session['responses']) != index:
+        flash("You tried to access an invalid qeustion")
+        return redirect(f"/questions/{len(session['responses'])}")
+
     question = survey.questions[index]
-    return render_template("question.html",
-                           question=question)
+    return render_template("question.html", question=question)
 
 
 @app.post("/answer")
 def handle_answer():
     """Keeps track of user answers and redirects to next question"""
-    RESPONSES.append(request.form['answer'])
-    next_question_index = len(RESPONSES)
+
+    responses = session['responses']
+    responses.append(request.form['answer'])
+    session['responses'] = responses
+
+    next_question_index = len(session['responses'])
 
     return redirect(f"/questions/{next_question_index}")
 
@@ -49,4 +59,8 @@ def handle_answer():
 @app.get("/thank_you")
 def create_thank_you():
     """Creates a thank you page with the answered questions"""
-    return render_template("completion.html", responses=RESPONSES, questions=survey.questions)
+    return render_template(
+        "completion.html",
+        responses=session['responses'],
+        questions=survey.questions
+        )
