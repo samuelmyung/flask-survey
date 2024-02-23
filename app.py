62bab1c975bbbe6c9702c4beb6e56b8138878b14
Samuel Myung <shmyung4@gmail.com>
2024-02-22 16:45:49 -0800

    Completed the thank you page

--- app.py
+++ app.py
@@ -10,32 +10,43 @@ debug = DebugToolbarExtension(app)
 
 RESPONSES = []
 
+
 @app.get("/")
-def render_homepage():
-    return render_template("survey_start.html", survey_title = survey.title,
-                           survey_instructions = survey.instructions)
+def create_homepage():
+    """Create homepage with a start of a survey"""
+    RESPONSES.clear()
+    return render_template("survey_start.html", survey_title=survey.title,
+                           survey_instructions=survey.instructions)
+
 
 @app.post("/begin")
 def change_page():
+    """Redirects us to the first question"""
     return redirect("/questions/0")
 
-@app.get("/questions/<int:index>")
-def render_question_page(index):
 
+@app.get("/questions/<int:index>")
+def create_question_page(index):
+    """Create each subsequent question page and redirects to thank you page if
+    no more questions"""
     if index >= len(survey.questions):
         return redirect("/thank_you")
 
     question = survey.questions[index]
     return render_template("question.html",
-                                question = question)
+                           question=question)
+
 
 @app.post("/answer")
 def handle_answer():
+    """Keeps track of user answers and redirects to next question"""
     RESPONSES.append(request.form['answer'])
     next_question_index = len(RESPONSES)
 
     return redirect(f"/questions/{next_question_index}")
 
+
 @app.get("/thank_you")
-def render_thank_you():
-    return render_template("completion.html", responses=RESPONSES, questions = survey.questions)
+def create_thank_you():
+    """Creates a thank you page with the answered questions"""
+    return render_template("completion.html", responses=RESPONSES, questions=survey.questions)
